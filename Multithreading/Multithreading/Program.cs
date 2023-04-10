c1664d45d95f9ae8474813339b9fc8bcc3230976
Artem <112962978+schultz1425@users.noreply.github.com>
2023-04-10 10:09:24 +0200

    Add files via upload
    Multithreading

---
+++ Multithreading/Multithreading/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace Multithreading
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            File.WriteAllText(SaveAsThread.path, string.Empty);
+
+            int[] arr1 = { 1, 2, 3 };
+            int[] arr2 = { 4, 5, 6 };
+            int[] arr3 = { 7, 8, 9 };
+
+            Thread t1 = new Thread(() => SaveAsThread.SaveArrayToFile(arr1, 1000));
+            Thread t2 = new Thread(() => SaveAsThread.SaveArrayToFile(arr2, 300));
+            Thread t3 = new Thread(() => SaveAsThread.SaveArrayToFile(arr3, 500));
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+
+            Thread.Sleep(2000);
+
+            Console.WriteLine("\nAll arrays have been saved to file. Press any button to open file");
+            Console.ReadKey();
+
+            Process.Start("notepad.exe", SaveAsThread.path);
+        }
+    }
+}
---
+++ Multithreading/Multithreading/SaveAsThread.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Multithreading
+{
+    public static class SaveAsThread
+    {
+        public static string path = "arrays.txt";
+        
+        public static void SaveArrayToFile(int[] arr, int sec)
+        {
+            lock (path)
+            {
+                Thread.Sleep(sec);
+                using (StreamWriter sw = File.AppendText(path))
+                {
+                    foreach (int num in arr)
+                    {
+                        sw.Write(num + " ");
+                        Console.Write(num + " ");
+                    }
+                    Console.WriteLine();
+                    sw.WriteLine();
+                }
+            }
+        }
+    }
+}
